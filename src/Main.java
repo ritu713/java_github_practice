88a51902ec380d48aac538cc22171da57eec57a7
Ritu <singh.ritu.rajiv@gmail.com>
2024-11-04 14:11:50 +0530

    Command line interface added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         ExchangeRate calc = new ExchangeRate();
-        System.out.println(calc.findExchangeRate("USD", "INR"));
+        calc.call();
     }
 }
