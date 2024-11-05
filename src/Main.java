2e4f15a43048e013f20f1a8ddc187b0eaf525fa2
Ritu <singh.ritu.rajiv@gmail.com>
2024-11-05 15:58:26 +0530

    Clean

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,5 @@ public class Main {
     public static void main(String[] args) {
         ExchangeRate calc = new ExchangeRate();
         calc.call();
-        System.out.println("Some error #3");
     }
 }
